c8e6e3743cd8338327c4262fc817f088ad89eefb
RakeshInfy <rksaini11291@gmail.com>
2020-07-24 12:00:38 +0530

    Updated java docs and removed unsued java library from pom.xml

--- src/main/java/com/java/controller/CustomerController.java
+++ src/main/java/com/java/controller/CustomerController.java
@@ -15,26 +15,45 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    /**
+     * Method to create a new record in customer document
+     * @param customer
+     */
     @PostMapping(value = "/")
     public void create(@RequestBody Customer customer){
         customerService.createCustomer(customer);
     }
 
+    /**
+     * Method to get all customers details from document
+     */
     @GetMapping(value = "/", produces = "application/json")
     public List<Customer> getAll(){
         return customerService.getAllCustomer();
     }
 
+    /**
+     * Method to update a record in customer document
+     * @param customer
+     */
     @PutMapping(value = "/", consumes = "application/json", produces = "application/json")
     public void update(@RequestBody Customer customer){
         customerService.updateCustomer(customer);
     }
 
+    /**
+     * Method to get record from customer document
+     * @param customer
+     */
     @GetMapping(value = "/{customernumber}")
     public Optional<Customer> get(@PathVariable int customernumber){
         return customerService.findCustomerById(customernumber);
     }
 
+    /**
+     * Method to delete a record in customer document
+     * @param customer
+     */
     @DeleteMapping(value ="/{customernumber}", produces = "application/json")
     public void delete(@PathVariable int customernumber) {
         customerService.deleteCustomerById(customernumber);
